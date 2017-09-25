81ccdb8bcbe140798f7f1bf22408fcd294ea25bf
Anoja Rajalakshmi <anojalax@gmail.com>
2017-09-24 20:57:11 -0700

    nit manip

---
+++ power_of_2_bit_manip/power_of_2_bit_manip/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  power_of_2_bit_manip
+//
+//  Created by Anoja Rajalakshmi on 9/22/17.
+//  Copyright © 2017 Anoja Rajalakshmi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
